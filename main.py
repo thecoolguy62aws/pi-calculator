8a7decd380eb5af43b816a027d8d6cb3b690c8ee
thecoolguy62aws <150736455+thecoolguy62aws@users.noreply.github.com>
2026-01-08 18:14:33 -0800

    Add support for only 1 digit on web GUI

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from decimal import Decimal, getcontext
 def isNum(s):
     try:
         s = int(s)
-        if s >= 2 and s <= 5000000: return True
+        if s >= 1 and s <= 5000000: return True
         else: return False
     except ValueError:
         return False
@@ -19,9 +19,16 @@ def getpi(event):
     
     if isNum(digits):
         getcontext().prec = int(digits)
-        pi = sum(1/Decimal(16)**k * (Decimal(4)/(8*k+1) - Decimal(2)/(8*k+4) - Decimal(1)/(8*k+5) - Decimal(1)/(8*k+6)) for k in range(100))
-        formatteddigits = "{:,}".format(int(digits))
-        output_div.innerText = f"Here's your {formatteddigits} digits of pi:"
+
+        if digits == 1 or digits == "1":
+            pi = "3"
+        else:
+            pi = sum(1/Decimal(16)**k * (Decimal(4)/(8*k+1) - Decimal(2)/(8*k+4) - Decimal(1)/(8*k+5) - Decimal(1)/(8*k+6)) for k in range(100))
+        formatteddigits = str("{:,}".format(int(digits)))
+        if int(digits) > 1:
+            output_div.innerText = f"Here's your {formatteddigits} digits of pi:"
+        else:
+            output_div.innerText = f"Here's your {formatteddigits} digit of pi:"
         pi_div.innerText = f"{pi}"
     elif str(digits) != "":
         output_div.innerText = f"Please enter a valid number that is at least 2 and at most 5,000,000."
